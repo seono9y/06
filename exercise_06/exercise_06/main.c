3ac2e1a95caf384deb72bd7a24bfaf21d5946758
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-12 15:25:24 +0900

    01-1

--- exercise_06/exercise_06/main.c
+++ exercise_06/exercise_06/main.c
@@ -7,18 +7,16 @@
 
 #include <stdio.h>
 
-void print_star(){
-    int i;
-    for(i=0; i<10; i++)
-        printf("*");
-}
-
 int main(void)
 {
-    print_star();
-    print_star();
-    print_star();
+    int i;
+    
+    for(i=0; i<10; i++)
+        printf("*");
+    for(i=0; i<10; i++)
+        printf("*");
+    for(i=0; i<10; i++)
+        printf("*");
     
     return 0;
-    
 }
