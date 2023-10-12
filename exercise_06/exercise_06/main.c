c452d5f14a232a5bfa5637358d1bf8fec7177962
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-12 15:22:56 +0900

    01-2

---
+++ exercise_06/exercise_06/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  exercise_06
+//
+//  Created by 추서연 on 2023/10/12.
+//
+
+#include <stdio.h>
+
+void print_star(){
+    int i;
+    for(i=0; i<10; i++)
+        printf("*");
+}
+
+int main(void)
+{
+    print_star();
+    print_star();
+    print_star();
+    
+    return 0;
+    
+}
