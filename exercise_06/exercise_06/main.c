7f12cf72cc82d80d632f54a50602c12858cb3891
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-12 16:50:34 +0900

    05-thinking

--- exercise_06/exercise_06/main.c
+++ exercise_06/exercise_06/main.c
@@ -5,17 +5,37 @@
 //  Created by 추서연 on 2023/10/12.
 //
 
+
 #include <stdio.h>
 
-int square(int a)
-{
-    return(a*a);
+
+int combination(int n, int r);
+int factorial(int c);
+
+int main(void) {
+    int n, r;
+
+    printf("input n: ");
+    scanf("%d", &n);
+    printf("input r: ");
+    scanf("%d", &r);
+
+    int result = combination(n, r);
+    printf("C(%d, %d) = %d\n", n, r, result);
+
+    return 0;
 }
 
-int main()
-{
-    int a=2;
-    a=square(a);
-    printf("a=%i\n",a);
+int combination(int n, int r) {
+    int a = factorial(n);
+    int b = factorial(r) * factorial(n - r);
+    return a / b;
 }
 
+int factorial(int c) {
+    int res = 1;
+    for (int i = 1; i <= c; i++) {
+        res = res * i;
+    }
+    return res;
+}
