ab4164f7641ec5b0c75928297ffaa31ecb82cb99
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-12 16:10:06 +0900

    03

--- exercise_06/exercise_06/main.c
+++ exercise_06/exercise_06/main.c
@@ -7,15 +7,41 @@
 
 #include <stdio.h>
 
-void func1(void){
-    int x;
-    printf("func1 x is at %p\n", &x);
+
+
+int sumTwo(int a, int b) {
+    int c = a + b;
+    return c;
 }
 
-int main(void){
-    int x;
-    printf("main x is at %p\n", &x);
-    func1();
-    
-    return 0;
+int square(int n)
+{
+    return n*n;
 }
+
+int get_max(int x, int y)
+{
+    if (x>y)
+        return x;
+        
+    return y;
+}
+
+int main(void)
+{
+    int a,b;
+    int n;
+    int x,y;
+    
+    a = 3;
+    b = 10;
+    printf("sumTwo result : %i\n", sumTwo(a,b));
+    
+    n = 4;
+    printf("square result : %i\n", square(n));
+    
+    x = 7;
+    y = 12;
+    printf("get_max result : %i\n", get_max(x, y));
+}
+
