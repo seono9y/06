7ccf6570987940d4455d40460da72f2676429791
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-12 15:42:46 +0900

    02

--- exercise_06/exercise_06/main.c
+++ exercise_06/exercise_06/main.c
@@ -7,16 +7,15 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-    int i;
-    
-    for(i=0; i<10; i++)
-        printf("*");
-    for(i=0; i<10; i++)
-        printf("*");
-    for(i=0; i<10; i++)
-        printf("*");
+void func1(void){
+    int x;
+    printf("func1 x is at %p\n", &x);
+}
+
+int main(void){
+    int x;
+    printf("main x is at %p\n", &x);
+    func1();
     
     return 0;
 }
